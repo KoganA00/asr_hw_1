d1bfbec747eda81e5970a9f6dd6b4ef273afd17a
KoganA00 <yarolika00@mail.ru>
2021-10-26 21:59:01 +0300

    fixing beam search test

--- asr_project_template-hw_asr_2021/hw_asr/augmentations/wave_augmentations/NormalNoise.py
+++ asr_project_template-hw_asr_2021/hw_asr/augmentations/wave_augmentations/NormalNoise.py
@@ -6,7 +6,7 @@ from hw_asr.augmentations.base import AugmentationBase
 
 
 class NormalNoise(AugmentationBase):
-    def __init__(self, min_f_decay=0,  *args, **kwargs):
+    def __init__(self, mean_=0.0, std_=0.05,  *args, **kwargs):
         self.noiser = AddColoredNoise(mean_, std_)
 
     def __call__(self, data: Tensor):
